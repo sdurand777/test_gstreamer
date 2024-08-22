0a2c3f376dbb1baa4811af34b482a51dd594b436
ivm <stan777.durand777@gmail.com>
2024-08-22 15:35:37 +0200

    update correction receiver

--- receiver.sh
+++ receiver.sh
@@ -1,1 +1,1 @@
-gst-launch-1.0 -v udpsrc port=5000 caps="application/x-rtp, media=video, clock-rate=90000, encoding-name=H264" ! rtph264depay ! avdec_h264 ! autovideosink
+gst-launch-1.0 -v udpsrc port=5000 caps="application/x-rtp, encoding-name=H264, payload=96" ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink
